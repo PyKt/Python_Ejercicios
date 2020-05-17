e063413ac0639e33fa57226a9b4e06f983d0ce5b
Pykt <5fdxYuAP>
2020-05-16 21:30:43 -0400

    update

--- generador_de_numeros.py
+++ generador_de_numeros.py
@@ -11,18 +11,20 @@ personalizado = fecha.strftime("El %D a las - %H:%M:%S\n\n")
 arch = open("Resultado.txt", "a+")
 arch.write(f"Creado {personalizado}")
 arch.write("#####Resultado Nacional#####\n")
-for lot in range(3):
 
-    lot = random.randint(1,100)
-    arch = open(f"Resultado.txt", "a+")
-    arch.write(f"{lot}\n")
-    print(lot)
+def juego():
+    for lot in range(3):
+        lot = random.randint(1,100)
+        arch = open(f"Resultado.txt", "a+")
+        arch.write(f"{lot}\n")
+        print(lot)
 
-arch.write("#####Resultado Leidsa#####\n")
-for led in range(7):
-
-    led = random.randint(1,38)
-    arch = open("Resultado.txt", "a+")
-    arch.write(f"{led}\n")
-    print(led)
-arch.closed
+    arch.write("#####Resultado Leidsa#####\n")
+    
+    for led in range(7):
+        led = random.randint(1,38)
+        arch = open("Resultado.txt", "a+")
+        arch.write(f"{led}\n")
+        print(led)
+    arch.closed
+salida = juego()
